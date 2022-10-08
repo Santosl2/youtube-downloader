a5b9b619e1c07e78da389cb033729561df071567
Santosl2 <mfilype2019@gmail.com>
2022-10-08 17:11:50 -0300

    feat: vite build change

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,20 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  experimental: {
+    renderBuiltUrl(
+      filename: string,
+      {
+        hostId,
+        hostType,
+        type,
+      }: {
+        hostId: string;
+        hostType: "js" | "css" | "html";
+        type: "public" | "asset";
+      }
+    ) {
+      return `./${filename}`;
+    },
+  },
 });
